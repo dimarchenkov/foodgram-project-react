ad27e0a96e065c5b275dbe5aa5f489984fdddfb5
Dmitry <dimarchenkov@yandex.ru>
2023-12-10 02:45:12 +0300

    Fix filter 2.1

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,7 @@ from django_filters.rest_framework import FilterSet
 from django_filters.rest_framework import filters as djangofilters
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
 
 
 class IngredientSearchFilter(SearchFilter):
