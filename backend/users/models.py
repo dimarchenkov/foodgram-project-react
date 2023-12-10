2fbe5fe6f575878264658246dfffc8c7effda26c
Dmitry <dimarchenkov@yandex.ru>
2023-12-10 17:12:02 +0300

    Fix 7.2

--- backend/users/models.py
+++ backend/users/models.py
@@ -117,6 +117,10 @@ class CustomUser(AbstractBaseUser):
         "Does the user have a specific permission?"
         return True
 
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
     @property
     def is_staff(self):
         "Is the user a member of staff?"
