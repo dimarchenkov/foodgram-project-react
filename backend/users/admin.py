b3d25b0f615c96b70528888f1d15c01c73c9005f
Dmitry <dimarchenkov@yandex.ru>
2023-12-10 17:03:07 +0300

    Fix 7

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,38 +2,64 @@
 Настойки интерфейса панели администратора.
 """
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from rest_framework.authtoken.models import TokenProxy
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+# from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import CustomUser, Subscription
 
 
-class CustomUserCreationForm(UserCreationForm):
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
     class Meta:
         model = CustomUser
-        fields = (
-            'email',
-            'username',
-            'first_name',
-            'last_name',
-            'is_active',
-            'is_staff'
-        )
+        fields = ['email', 'username', 'first_name', 'last_name']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
-class CustomUserChangeForm(UserChangeForm):
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
     class Meta:
         model = CustomUser
-        fields = (
+        fields = [
             'email',
             'username',
-            'first_name',
+            'password',
             'last_name',
+            'first_name',
             'is_active',
-            'is_staff'
-        )
+            'is_admin'
+        ]
 
 
 class SubscriptionOnInline(admin.StackedInline):
@@ -47,11 +73,10 @@ class SubscriptionInline(admin.StackedInline):
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     """Регистрация юзера."""
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
+    form = UserChangeForm
+    add_form = UserCreationForm
 
     list_display = (
         'id',
@@ -59,27 +84,19 @@ class CustomUserAdmin(UserAdmin):
         'username',
         'first_name',
         'last_name',
-        'is_staff',
+        'is_admin',
     )
     list_filter = (
         'email',
         'username',
     )
-    search_fields = (
-        'username',
-        'email',
-        'first_name',
-        'last_name'
-    )
-    inlines = [SubscriptionOnInline, SubscriptionInline]
-
     fieldsets = (
         (None, {'fields': (
             'email', 'username', 'first_name', 'last_name', 'password',
         )}),
         ('Permissions', {'fields': (
             'is_active',
-            'is_staff',
+            'is_admin',
             'is_superuser',
         )})
     )
@@ -88,10 +105,18 @@ class CustomUserAdmin(UserAdmin):
             'classes': ('wide',),
             'fields': (
                 'email', 'username', 'first_name', 'last_name', 'password1',
-                'password2', 'is_active', 'is_staff', 'is_superuser',
+                'password2', 'is_active', 'is_admin', 'is_superuser',
             )
         }),
     )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    inlines = [SubscriptionOnInline, SubscriptionInline]
+    filter_horizontal = []
 
 
 @admin.register(Subscription)
--- backend/users/models.py
+++ backend/users/models.py
@@ -9,7 +9,42 @@ from django.db.models import Q, F
 from foodgram_backend import constants
 
 
-class CustomUser(AbstractUser):
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email, username, password, **extra_fields
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
     """
     Модель пользователя.
 
@@ -37,6 +72,8 @@ class CustomUser(AbstractUser):
     """
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyUserManager()
 
     first_name = models.CharField(
         max_length=constants.FIRST_NAME_MAX_LENGTH,
@@ -70,12 +107,23 @@ class CustomUser(AbstractUser):
             UnicodeUsernameValidator()
         ],
     )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
     class Meta:
         """Класс Meta модели User."""
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         ordering = ['username']
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
 
     def __str__(self):
         return self.username
